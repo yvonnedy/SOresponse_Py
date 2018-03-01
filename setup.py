9bd60e6fddf2188c39bd31d5a4a8864f25974cd8
yvonnedy <kiiimberly@126.com>
2018-03-01 15:08:44 -0800

    add the setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'SOresponse_Py',
+    version = '0.1.0',
+    author = 'Ted Thompson, Fang Yang, Ying Dong',
+    packages = ['SOresponse_Py'],
+    scripts = ['SOresponse_Py/web_page.py', 'SOresponse_Py/popular.py', 'SOresponse_Py/response_stats.py'],
+    url = 'https://github.com/UBC-MDS/SOresponse_Py',
+    license = 'LICENSE',
+    description = 'Analysis of Stack Overflow responses',
+    long_description = open('README.md').read(),
+    install_requires = [],
+)
