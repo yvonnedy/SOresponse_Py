cb39edb980f182890b080183a1b9679d20853a11
yvonnedy <kiiimberly@126.com>
2018-03-17 16:31:05 -0700

    update test result

--- SOresponse_Py/test/test_response_stats.py
+++ SOresponse_Py/test/test_response_stats.py
@@ -21,7 +21,7 @@ def test_result():
 
       obs3 = response_stats('https://stackoverflow.com/questions/49206233/how-can-i-convert-this-date-03-01-2018-1200-am-to-2018-03-01-in-c')
       exp3 = {"Author's reputation score": 1,
-              'Average reputation score': 66994,
+              'Average reputation score': 66997,
               'Number of responses': 5,
               'Top reputation score': 530000}
       assert obs3 == exp3
